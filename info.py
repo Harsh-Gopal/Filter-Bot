4df3ae6a327ed98e076b618c77cb6cb3c0f3d25e
RituRajPS <riturajpsofficial@gmail.com>
2022-09-02 16:52:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -33,8 +33,8 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://riturajps:<password>@riturajps.omoivzo.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Piratedcity")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Piratedcity_filterbot')
+DATABASE_NAME = environ.get('DATABASE_NAME', "PiracyHunt")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'PiracyHunt')
 
 # Others
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))

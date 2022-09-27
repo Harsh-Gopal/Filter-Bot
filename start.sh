14ca028e6273f711e498d1862a35ee2d002ca65e
RituRajPS <riturajpsofficial@gmail.com>
2022-09-27 16:00:02 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/riturajps-india/Auto-Filter-Bot.git /Auto-Filter-Bot
+  git clone https://github.com/RituRajPS-Official/Auto-Filter-Bot.git /Auto-Filter-Bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Auto-Filter-Bot

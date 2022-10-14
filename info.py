30c7dffe5e212518b4b4dd58e0c7460a2a37b79f
RituRajPS <riturajpsofficial@gmail.com>
2022-10-14 16:16:07 +0530

    Update info.py

--- info.py
+++ info.py
@@ -19,7 +19,7 @@ BOT_TOKEN = environ['BOT_TOKEN']
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
-PICS = (environ.get('PICS', 'https://telegra.ph/file/125cde400fa3eeb362562.jpg')).split()
+PICS = (environ.get('PICS', 'https://riturajps-official.netlify.app/riturajps_files/Script/File/Upload/Image/autofilterbot.jpg')).split()
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
